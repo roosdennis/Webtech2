febfe1f9e375e70fa320c20b9bc408a25db024e2
roosdennis <dennisroos87@gmail.com>
2024-09-30 10:02:20 +0200

    Opdracht 2 met bootstrap en onnodige comments weg

--- Opdracht2/public/index.php
+++ Opdracht2/public/index.php
@@ -10,14 +10,6 @@ define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 /* HIER CODE (zie de instructies op Blackboard) */
 require APP_PATH . 'app.php';
 $files = getTransactionFiles(FILES_PATH);
-//
-//$transactions= [];
-//foreach ($files as $file){
-//    $transactions[] = array_merge($transactions, getTransactions($file));
-//}
-//
-//print("<br><br><br><br><br><br><br><br><br><br><br><br>");
-//require VIEWS_PATH . 'transactions.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,17 +17,23 @@ $files = getTransactionFiles(FILES_PATH);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transactiebestanden</title>
+    <!-- Bootstrap CSS toevoegen -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<h1>Klik op een bestand om de transacties te bekijken</h1>
-<ul>
-    <?php foreach ($files as $file): ?>
-        <li>
-            <a href="../views/transactions.php?file=<?php echo urlencode(basename($file)); ?>">
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Transactiebestanden</h1>
+    <div class="d-flex flex-column align-items-center">
+        <?php foreach ($files as $file): ?>
+            <a href="../views/transactions.php?file=<?php echo urlencode(basename($file)); ?>" class="btn btn-outline-primary mb-2 w-50">
                 <?php echo basename($file); ?>
             </a>
-        </li>
-    <?php endforeach; ?>
-</ul>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<!-- Bootstrap JS en Popper.js toevoegen -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
--- Opdracht2/views/transactions.php
+++ Opdracht2/views/transactions.php
@@ -25,78 +25,83 @@ if (isset($_GET['file'])) {
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transacties</title>
+    <!-- Bootstrap CSS toevoegen -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
+        /* Aangepaste styling voor de tabel */
         table {
-            width: 100%;
+            margin-top: 20px;
             border-collapse: collapse;
-            text-align: center;
+            width: 100%;
         }
-
         table tr th, table tr td {
-            padding: 5px;
-            border: 1px #eee solid;
-        }
-
-        tfoot tr th, tfoot tr td {
-            font-size: 20px;
-        }
-
-        tfoot tr th {
-            text-align: right;
+            padding: 12px;
+            border: 1px solid #ddd;
         }
     </style>
 </head>
 <body>
-<h1>Transacties voor bestand: <?php echo htmlspecialchars($fileName); ?></h1>
-<table>
-    <thead>
-    <tr>
-        <th>Datum</th>
-        <th>Check #</th>
-        <th>Beschrijving</th>
-        <th>Bedrag</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php if (!empty($transactions)): ?>
-        <?php foreach ($transactions as $transaction): ?>
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Transacties voor bestand: <?php echo htmlspecialchars($fileName); ?></h1>
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped">
+            <thead class="thead-dark">
             <tr>
-                <td><?php echo htmlspecialchars($transaction[0]); ?></td>
-                <td><?php echo htmlspecialchars($transaction[1]); ?></td>
-                <td><?php echo htmlspecialchars($transaction[2]); ?></td>
-                <td style="color: <?php echo ($transaction[3] >= 0) ? 'green' : 'red'; ?>">
-                    <?php echo htmlspecialchars($transaction[3]); ?>
+                <th>Datum</th>
+                <th>Check #</th>
+                <th>Beschrijving</th>
+                <th>Bedrag</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if (!empty($transactions)): ?>
+                <?php foreach ($transactions as $transaction): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($transaction[0]); ?></td>
+                        <td><?php echo htmlspecialchars($transaction[1]); ?></td>
+                        <td><?php echo htmlspecialchars($transaction[2]); ?></td>
+                        <td style="color: <?php echo ($transaction[3] >= 0) ? 'green' : 'red'; ?>">
+                            <?php echo htmlspecialchars($transaction[3]); ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr><td colspan="4">Geen transacties gevonden</td></tr>
+            <?php endif; ?>
+            </tbody>
+            <tfoot>
+            <tr>
+                <th colspan="3">Totale Inkomsten:</th>
+                <td><?php echo number_format(totalIncome([$transactions]), 2, ',', '.'); ?></td>
+            </tr>
+            <tr>
+                <th colspan="3">Totale Uitgaven:</th>
+                <td><?php echo number_format(totalExpenses([$transactions]), 2, ',', '.'); ?></td>
+            </tr>
+            <tr>
+                <th colspan="3">Netto totaal:</th>
+                <?php $netTotal = totalIncome([$transactions]) + totalExpenses([$transactions]); ?>
+                <td style="color: <?php echo ($netTotal >= 0) ? 'green' : 'red'; ?>">
+                    <?php echo number_format($netTotal, 2, ',', '.'); ?>
                 </td>
             </tr>
-        <?php endforeach; ?>
-    <?php else: ?>
-        <tr><td colspan="4">Geen transacties gevonden</td></tr>
-    <?php endif; ?>
-    </tbody>
-    <tfoot>
-    <tr>
-        <th colspan="3">Totale Inkomsten:</th>
-        <td><?php echo number_format(totalIncome([$transactions]), 2, ',', '.'); ?></td>
-    </tr>
-    <tr>
-        <th colspan="3">Totale Uitgaven:</th>
-        <td><?php echo number_format(totalExpenses([$transactions]), 2, ',', '.'); ?></td>
-    </tr>
-    <tr>
-        <th colspan="3">Netto totaal:</th>
-        <?php $netTotal = totalIncome([$transactions]) + totalExpenses([$transactions]); ?>
-        <td style="color: <?php echo ($netTotal >= 0) ? 'green' : 'red'; ?>">
-            <?php echo number_format($netTotal, 2, ',', '.'); ?>
-        </td>
-    </tr>
-    </tfoot>
-</table>
+            </tfoot>
+        </table>
+    </div>
+    <!-- Knop om terug te gaan naar de hoofdpagina -->
+    <div class="text-center mt-4">
+        <a href="../public/index.php" class="btn btn-primary">Terug naar hoofdpagina</a>
+    </div>
+</div>
+
+<!-- Bootstrap JS en Popper.js toevoegen -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
